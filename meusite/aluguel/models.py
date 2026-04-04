1f13b903e50390e7971cfd45bf8da9a1c59a19d2
vini <viniiciius.monteiro@gmail.com>
2026-04-03 22:47:29 -0300

    Criação das funções de disponibilidade do modelo Reserva

--- meusite/aluguel/models.py
+++ meusite/aluguel/models.py
@@ -8,7 +8,7 @@ class Carrinho(models.Model):
     status = models.BooleanField(default=True)
 
     def __str__(self):
-        return self.id
+        return str(self.id)
 
 class Sorvete(models.Model):
     nome_sorvete = models.CharField(max_length=200)
@@ -58,22 +58,52 @@ class Reserva(models.Model):
 
     def subtotal_sorvetes(self):
         """Calcula APENAS o valor dos produtos escolhidos."""
-        total = sum(item.quantidade * item.sorvete.preco for item in self.itens_pedido.all())
+        total = sum(item.quantidade_escolhida * item.id_sorvete.preco for item in self.itens.all())
         return Decimal(total)
     
     def taxa_aluguel(self):
         """Calcula APENAS a taxa, aplicando a regra de gratuidade."""
         if self.subtotal_sorvetes() >= 300:
             return Decimal('0.00')
-        return self.carrinho.preco_diaria
+        return self.id_carrinho.preco_diaria
     
     def total_pedido(self):
         """Soma as duas partes para dar o valor final ao cliente."""
         return self.subtotal_sorvetes() + self.taxa_aluguel()
 
+    @classmethod
+    def vagas_disponiveis(cls, data_desejada):
+        """
+        Retorna a quantidade de carrinhos livres para uma data específica.
+        """
+        total_carrinho = Carrinho.objects.filter(status=True).count()
+        alugueis_confirmados = cls.objects.filter(
+            data_evento = data_desejada,
+            status='confirmado'
+        ).count()
+
+        return total_carrinho - alugueis_confirmados
+    
+    def pode_confirmar(self):
+        return self.vagas_disponiveis(self.data_evento) > 0
+
+    def verificar_alerta_estoque(self):
+        """
+        Apenas avisa se HOJE não haveria estoque, 
+        mas permite o pedido se a data for distante.
+        """
+        alertas = []
+        for item in self.itens.all():
+            if item.quantidade_escolhida > item.id_sorvete.quantidade:
+                diferenca = item.quantidade_escolhida - item.id_sorvete.quantidade
+                alertas.append(f"Faltam {diferenca} unidades de {item.id_sorvete.nome_sorvete}")
+        return alertas
+
     def __str__(self):
-        return self.id
+        return f"Reserva {self.id} - {self.id_cliente.nome_cliente}"
     
 class ReservaProduto(models.Model):
-    id_reserva = models.ForeignKey(Reserva, on_delete=models.CASCADE)
-    id_sorvete = models.ManyToManyField(Sorvete, related_name="sorvetes")
+    # Atualização adicionada quantidade_escolhida
+    id_reserva = models.ForeignKey(Reserva, on_delete=models.CASCADE, related_name="itens")
+    id_sorvete = models.ForeignKey(Sorvete, on_delete=models.CASCADE) # Um por linha
+    quantidade_escolhida = models.IntegerField(default=1)
